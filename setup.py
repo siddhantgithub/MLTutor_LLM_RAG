ad0d01ac571d3aab92b3d68ce901bef67482281b
siddhantgithub <siddhant.tewari@gmail.com>
2024-02-08 10:50:22 -0600

    CookieCutter template added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A virtual teacher to teach Machine Learning using LLM and Generative AI. The solution uses RAG system built using Mistral 7B open source LLM',
+    author='Siddhant Tewari',
+    license='MIT',
+)
